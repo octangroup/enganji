b6d689d6e6ca92a9a606a22dd22eb984c697b950
Promesse <promessed97@gmail.com>
2019-03-18 13:06:14 +0200

    Apply delete and update functions in index page of deals
    Applying the delete and update functions in the blade so that
    we can do both updating and deleting the deal

--- resources/views/affiliate/deals/index.blade.php
+++ resources/views/affiliate/deals/index.blade.php
@@ -8,16 +8,21 @@
              <button class="btn btn-primary toggler" data-toggle="#add-deal-form">{{_('Add')}}</button>
          </div>
 
-         @foreach($products as $product)
-
          <div id="add-deal-form" class="card-body hidden-temp">
              <form action="{{action('Affiliate\DealsController@store')}}" method="post">
                  {{csrf_field()}}
              <div class="row">
                  <div class="col-md-5">
                      <label>Name</label>
-                     <input value="{{$product->name}}" class="form-control" >
-                     <input value="{{$product->id}}" type="hidden" name="product_id">
+
+                     <select class="form-control" name="product_id">
+                         <option disabled selected> choose product</option>
+                       @foreach($products as $product)
+                           <option value="{{$product->id}}">{{$product->name}}</option>
+                           @endforeach
+                     </select>
+                     {{--<input value="{{$product->name}}" class="form-control" >--}}
+                     {{--<input value="{{$product->id}}" type="hidden" name="product_id">--}}
                  </div>
 
                  <div class="col-md-5">
@@ -45,7 +50,7 @@
              </form>
          </div>
 
-             @endforeach
+
          <br>
          @foreach($deals as $deal)
              <div class="list-group-item">
@@ -56,7 +61,55 @@
                          <p><strong>End at: </strong> {{$deal->end_at}}</p>
                          <p><strong>Price: </strong> {{$deal->price}}</p>
                      </div>
+                     <div>
+                         <button class="btn btn-success  toggler" data-toggle="#mod-deal-form{{$deal->id}}">Modify</button>
+                         <a class="btn btn-danger" href="{{action('Affiliate\DealsController@delete',[$deal->id])}}">delete</a>
+                     </div>
+
+
                  </div>
+                 <div id="mod-deal-form{{$deal->id}}" class="card-body hidden-temp">
+                     <form action="{{action('Affiliate\DealsController@update',[$deal->id])}}" method="post">
+                         {{csrf_field()}}
+                         <div class="row">
+                             <div class="col-md-5">
+                                 <label>Name</label>
+
+                                 <select class="form-control" name="product_id">
+                                     <option disabled selected> choose product</option>
+                                     @foreach($products as $product)
+                                         <option value="{{$product->id}}">{{$product->name}}</option>
+                                     @endforeach
+                                 </select>
+                                 {{--<input value="{{$product->name}}" class="form-control" >--}}
+                                 {{--<input value="{{$product->id}}" type="hidden" name="product_id">--}}
+                             </div>
+
+                             <div class="col-md-5">
+                                 <label>New price</label>
+                                 <input class="form-control" value="{{$deal->price}}"  name="price">
+                             </div>
+
+                         </div>
+
+                         <div class="row">
+                             <div class="col-md-5">
+                                 <label>Begin</label>
+                                 <input class="form-control" type="date" name="begin_on">
+                             </div>
+
+                             <div class="col-md-5">
+                                 <label>End</label>
+                                 <input  class="form-control" type="date" name="end_at">
+                             </div>
+
+                         </div>
+                         <div>
+                             <button class="btn btn-primary mt-2">update</button>
+                         </div>
+                     </form>
+                 </div>
+
              </div>
                      @endforeach
      </div>
