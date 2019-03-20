03ab7a7feec3e80dfbe6dc3b248aa15cf6313eb9
Herve <ngenziherve@gmail.com>
2019-03-20 10:32:01 +0200

    Add category request to facilitate work
    Category request is created to easy the work

---
+++ app/Http/Requests/CategoryRequest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Category;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+
+class CategoryRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|string'
+        ];
+    }
+
+    public function createCategory(){
+
+        $category = new Category();
+        return $this->store($category);
+    }
+
+    protected function store(Category $category){
+        $category->name = $this->name;
+        $category->save();
+
+        return true;
+    }
+
+    public function update($id){
+        $category = Category::findorfail($id);
+        return $this->store($category);
+    }
+}
