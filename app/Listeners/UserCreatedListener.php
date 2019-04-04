a0b3e7c2dacd8a6465cec9941de1d862e75af30a
Promesse <promessed97@gmail.com>
2019-04-04 17:00:09 +0200

    Created user notification after registering
    Created notification of user after he registers he gets
    the email notification that tells him that he has successfully
    registered

---
+++ app/Listeners/UserCreatedListener.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Notifications\UserCreatedNotification;
+use App\User;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class UserCreatedListener
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  Registered  $event
+     * @return void
+     */
+    public function handle(Registered $event)
+    {
+        $user = User::findOrFail($event->user->id);
+
+        $user->notify(new UserCreatedNotification($event->user));
+    }
+}
