b91c89d472c44f11078cc7d2fcf3cf72d0fc6645
lewis <lewisdylannkuranga@gmail.com>
2019-05-28 12:11:12 +0200

    Fix chat functionality on the user's side
    Fixed the chatting functionality between the user and the
    affiliate to allow the user to chat to the affiliate that
    posted a certain product

---
+++ resources/views/admin/layouts/navbar.blade.php
@@ -0,0 +1,13 @@
+ {{--<div class="collapse navbar-collapse" id="navbarSupportedContent">--}}
+        {{--<!-- Left Side Of Navbar -->--}}
+        {{--<ul class="navbar-nav mr-auto">--}}
+            {{--@if (Auth::guard('admin')->check())--}}
+                {{--<li class="nav-item">--}}
+                    {{--<a class="nav-link" href="">--}}
+
+                    {{--</a>--}}
+                {{--</li>--}}
+
+    {{--</ul>--}}
+
+    {{--</div>--}}
