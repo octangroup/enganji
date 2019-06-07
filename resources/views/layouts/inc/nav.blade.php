61f1e8482d34d71bb9fdf1888be7bfe986e962d3
Arsene <karemarsy@gmail.com>
2019-06-07 15:15:35 +0200

    Responsiveness of the view product
    design the responsiveness of view product

--- resources/views/layouts/inc/nav.blade.php
+++ resources/views/layouts/inc/nav.blade.php
@@ -1,58 +1,57 @@
-<nav class=" w-90 mx-auto rounded-b-xlg xl:-mt-3  xs:py-3 mb-4 xl:py-4 xs:bg-white xl:bg-white-smoke ">
+<nav class=" w-90 mx-auto rounded-b-xlg xl:-mt-3   xl:mb-4 xl:py-4 xs:bg-white xl:bg-white-smoke ">
 
 
-    <div class="w-90 mx-auto flex flex-wrap">
-        <ul class="xs:w-90 md:w-80 xl:w-50 list my-0 mt-2  text-black-dark">
+    <div class="w-90 mx-auto flex flex-wrap ">
+        <ul data-toggle="#sidebar" class="w-10 md:w-40 xs:w-30 xl:hidden mt-3 list my-0  toggler text-black-dark">
+            <li class="pb-4 pt-1">
+                <i class="fi flaticon-menu text-black text-2xl"></i>
+            </li>
+        </ul>
+        <ul class="w-50 md:w-40 xs:w-50  list my-0 mt-2  text-black-dark">
             <li class="pb-4 pt-1">
                 <a href="{{action('HomeController@index')}}"
                    class="font-primary font-medium text-3xl inherit-color no-underline">
-                    <img src="{{asset('img/logo.png')}}" class="w-rem-32 absolute t-0 pt-2">
+                    <img src="{{asset('img/logo.png')}}" class="w-rem-32 mt-2 absolute t-0 pt-2">
                 </a>
             </li>
         </ul>
-        <ul data-toggle="#sidebar" class="w-10 md:w-20 xl:hidden list my-0 mt-2 toggler text-black-dark">
-            <li class="pb-4 pt-1">
-                <i class="fi flaticon-menu-4 text-black text-2xl"></i>
-            </li>
-        </ul>
-
-        <ul class="w-50 hidden xl:block list my-0 text-right text-black-dark">
-            <li class="text-xl mt-0 pt-0 w-60 mr-4 inline-block font-primary font-medium">
+        <ul class="w-50 md:w-20 xs:w-20 mx-3 xs:hidden md:hidden list my-0 text-right text-black-dark">
+            <li class="text-xl mt-0 md:mt-4  pt-0 w-60 mr-4 inline-block font-primary font-medium">
                 <form name="search_form" method="get" action="{{action('ProductsController@search')}}"
                       class="flex my-0">
                     <div class="flex-90">
                         <input name="keyword" type="text" placeholder="Search" value="{{$keyword ?? null}}"
                                class="text-right text-black-dark bg-transparent m-0 focus:shadow-none font-medium form-input border-none border-none p-0 ">
                     </div>
-                    <div class="flex-10 text-right p-0">
+                    <div class="flex-10 text-right xs:hidden p-0">
                         <button type="submit"
                                 class="btn bg-transparent shadow-none py-0 m-0 appearance-none -mt-2 hover:text-accent transition-250ms focus:text-primary">
                             <i
-                                class="fi flaticon-search  text-xl leading-0"></i></button>
+                                    class="fi flaticon-search  text-xl leading-0"></i></button>
                     </div>
                 </form>
             </li>
-            <li class="text-xl  mt-0 pt-2 mr-4 inline-block font-primary font-medium hover:text-accent transition-250ms">
+            <li class="text-xl xs:hidden md:hidden mt-0 pt-2 mr-4 inline-block font-primary font-medium hover:text-accent transition-250ms">
                 <a href="{{action('WishListController@index')}}" class="inherit-color no-underline">
                     <i class="fi flaticon-like-1"></i>
                 </a>
             </li>
-            <li class="text-xl  mt-0 pt-2 mr-4 inline-block font-primary font-medium hover:text-accent transition-250ms">
+            <li class="text-xl xs:hidden md:hidden mt-0 pt-2 mr-4 inline-block font-primary font-medium hover:text-accent transition-250ms">
                 <a href="{{action('CartController@index')}}" class="inherit-color no-underline">
                     <i class="fi flaticon-shopping-cart"></i>
                 </a>
 
             </li>
 
-            <li class="text-xl mt-0 pt-2 mr-4 inline-block font-primary font-medium hover:text-accent transition-250ms">
-                    <a href="{{action('ChatsController@index')}}" class="inherit-color no-underline">
-                        <i class="fi flaticon-chat"></i>
-                    </a>
+            <li class="text-xl xs:hidden md:hidden mt-0 pt-2 mr-4 inline-block font-primary font-medium hover:text-accent transition-250ms">
+                <a href="{{action('ChatsController@index')}}" class="inherit-color no-underline">
+                    <i class="fi flaticon-chat"></i>
+                </a>
             </li>
 
-            <li class="text-xl  mt-0 pt-2  inline-block font-primary font-medium hover:text-accent transition-250ms relative dropdown mx-0 z-999">
+            <li class="text-xl xs:hidden md:hidden  mt-0 pt-2  inline-block font-primary font-medium hover:text-accent transition-250ms relative dropdown mx-0 z-999">
                 <a class="inherit-color font-primary"><i class="fi flaticon-user"></i> <i
-                        class="fas fa-chevron-down text-xs"></i></a>
+                            class="fas fa-chevron-down text-xs"></i></a>
                 <div class="dropdown-content w-rem-74 mx-0 -ml-20 md:-ml-8 text-left font-roboto font-normal shadow mt-0
                                border-2 border-solid border-white cursor-pointer z-999 absolute">
                     <div class="w-100 absolute -t-2">
@@ -65,7 +64,7 @@
                             <p class="text-center text-xs bg-grey-lighter py-2">My Account</p>
                             <p><a href="{{action('ProfileController@index')}}"
                                   class="hover:bg-grey-lightest px-4 text-sm"><i
-                                        class="fi flaticon-user mr-2 text-base"></i> {{Auth::user()->name}}</a></p>
+                                            class="fi flaticon-user mr-2 text-base"></i> {{Auth::user()->name}}</a></p>
                             <p><a href="{{ route('logout') }}" dusk="logout-user"
                                   class="hover:bg-grey-lightest px-4 text-sm"
                                   onclick="event.preventDefault();
@@ -85,15 +84,35 @@
                             <p class="text-center text-xs bg-grey-lighter py-2">My Account</p>
                             <p><a href="{{route('login')}}"
                                   class="hover:bg-grey-lightest px-4 text-sm"><i
-                                        class="fi flaticon-login mr-2 text-base"></i> Sign in</a></p>
+                                            class="fi flaticon-login mr-2 text-base"></i> Sign in</a></p>
                             <p><a href="{{route('register')}}"
                                   class="hover:bg-grey-lightest px-4 text-sm"><i
-                                        class="fi flaticon-user-7 mr-2 text-base"></i> Sign up</a></p>
+                                            class="fi flaticon-user-7 mr-2 text-base"></i> Sign up</a></p>
                         </div>
                     @endguest
                 </div>
             </li>
         </ul>
+        <div data-toggle="#search" class="w-20 toggler mt-3 text-center hidden xs:block md:block  py-3 flex align-items-center justify-content-center ">
+            <p class="m-0 text-xl"><i class="fi flaticon-search"></i></p>
+        </div>
+        <div id="search" class="w-90 xl:hidden  text-center mx-auto py-3 hidden-temp " style="display: none;">
+            <form name="search_form" method="get" action="{{action('ProductsController@search')}}">
+                <div class="bg-white rounded-full border-1 border-solid border-grey-light w-85 mx-auto overflow-x-hidden  flex">
+                    <div class="w-80 md:w-90 sm:w-90">
+                        <input name="keyword" type="text" placeholder="Search.." value="" required="required" class="bg-transparent appearance-none outline-none border-none w-100 pl-4 m-0 py-2 ">
+                    </div>
+                    <div class="w-20 md:w-10 sm:w-10  pr-2">
+                        <button class="border-0 bg-transparent  text-black  text-center px-0 py-0 text-lg  h-100">
+                            <i class="fi flaticon-search "></i>
+                        </button>
+                    </div>
+                </div>
+            </form>
+        </div>
+
+
+
         <ul class="list w-100 hidden xl:block text-center text-black-dark   text-sm">
             @foreach(\App\Category::get()->take(6) as $category)
                 <li class="inline-block mx-3"><a
