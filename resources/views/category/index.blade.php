d29f9dfdc1c5e97d0fe682feda1fa85fcf0cc651
Arsene <karemarsy@gmail.com>
2019-06-18 17:58:11 +0200

    Design the category page
    Redesign the design of category on all devices

--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -3,15 +3,15 @@
 
     <div class="w-90 mx-auto">
 
-        <div class="w-100 mx-auto px-2 mt-2 flex">
-            <div class="w-25 ml-50">
+        <div class="w-100 mx-auto px-2 mt-2 xl:flex">
+            <div class="w-100 font-bold font-primary ml-50">
                 {{$categories->count()}} {{__('Categories')}}<br>
             </div>
 
-            <div class="">
+            <div class="bg-white-smoke w-100 border-1  rounded-xlg mt-3 border-solid border-grey-lighter">
                 @foreach($categories as $category)
-                    <div class="w-25 xs:w-100 mx-3 bg-blue my-5 xs:my-2 rounded-xlg inline-block ">
-                        <div class="text-center text-white">
+                    <div class="border-0 border-b-1 py-3 border-solid border-grey-lighter">
+                        <div class="text-center font-primary text-white">
                             <a href="{{action('CategoryController@show',[$category->id])}}">
                         {{$category->name}}
                             </a>
